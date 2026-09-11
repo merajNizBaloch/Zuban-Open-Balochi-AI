264232f075b1b907a6aaf8d46f876da1eb98a618
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-12 01:09:39 +0500

    brand: use bundled logo in footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { githubUrl } from "@/lib/site";
+import { ZubanLogo } from "@/components/zuban-logo";
 
 export function SiteFooter() {
   return (
@@ -7,14 +8,7 @@ export function SiteFooter() {
       <div className="culture-band" aria-hidden="true" />
       <div className="shell footer-simple">
         <div className="footer-brand">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            className="footer-logo"
-            src="/zuban-mark.png?v=1"
-            alt=""
-            width="72"
-            height="72"
-          />
+          <ZubanLogo className="footer-logo" width={72} height={72} />
           <div>
             <strong>Zubán</strong>
             <span>Open Balochi language technology.</span>
